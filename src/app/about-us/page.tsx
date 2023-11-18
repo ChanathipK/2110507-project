04d662c92988292ea750ce5f557225942538f528
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2023-11-18 14:49:09 +0700

    add empty about us

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutUs() {
+    return (
+        <div className="w-full h-[calc(100vh-5rem-400px)] sm:h-[calc(100vh-2.5rem-300px)] md:h-[calc(100vh-2.5rem-9rem)]">
+            <h1 className="pt-20 text-2xl text-center mb-4">Well, this page is just a blank page. I created this page just to prove that I know how routing in Next.js works.</h1>
+
+            <p className="text-center">How did I implement the navigation bar and the footer? I'll tell you.</p>
+        </div>
+    );
+}
