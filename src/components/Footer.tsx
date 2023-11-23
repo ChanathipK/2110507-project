82c1c83f259a15a8cea3b512df288c6e91a4eda1
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2023-11-23 09:01:40 +0700

    remove unused reponsive prefix in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="w-full h-[400px] max-h-[400px] sm:h-[300px] sm:max-h-[400px] md:h-36 relative bottom-0 left-0 bg-slate-600 flex justify-center items-center text-slate-50 overflow-hidden">
+        <footer className="w-full h-[400px] max-h-[400px] sm:h-[300px] md:h-36 relative bottom-0 left-0 bg-slate-600 flex justify-center items-center text-slate-50 overflow-hidden">
             <div className="flex flex-col w-2/5 md:w-1/5 items-center">
                 <Link
                 className="text-[12px] md:text-[16px]"
